e2601798af19c8c9782c6d1db963f758926a3486
rasmusekbombl <rasmus.ekbom@bjornlunden.se>
2023-12-30 21:21:39 +0100

    ff

---
+++ build/_app/immutable/chunks/paths.nDYPXJPl.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_bng8xc?.base??"/rasmusekbom.github.io",a=globalThis.__sveltekit_bng8xc?.assets??s;export{a,s as b};
--- svelte.config.js
+++ svelte.config.js
@@ -15,6 +15,7 @@ const config = {
 			pages: 'build',
 			assets: 'build',
 			fallback: null,
+			strict: false,
 		  }),
 		paths: {
             base: process.env.NODE_ENV === 'production' ? '/rasmusekbom.github.io' : '',
